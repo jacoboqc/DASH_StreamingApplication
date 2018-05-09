b34c50fe170918f33465023809b36135a4a3c925
Guiamrey <guimarey.r@gmail.com>
2018-05-09 20:24:55 +0200

    MPD error fixed

--- server/dash-video-mpd.sh
+++ server/dash-video-mpd.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# THIS SCRIPT CONVERTS EVERY MP4 (IN THE CURRENT FOLDER AND SUBFOLDER) TO A MULTI-BITRATE VIDEO IN MP4-DASH
-# For each file "videoname.mp4" it creates a folder "dash_videoname" containing a dash manifest file "stream.mpd" and subfolders containing video segments.
-# Explanation: 
-# https://rybakov.com/blog/
-
 tstart="$(date "+%s")"
 
 MYDIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
@@ -38,23 +33,28 @@ if [ ! -d "resources/${f}" ]; then #if directory does not exist, convert
     ffmpeg -y -i "${fe}" -vsync passthrough -s 480x270 -c:v libx264 -b:v 350k -x264opts keyint=25:min-keyint=25:no-scenecut -profile:v main -preset slow -movflags +faststart -c:a aac -b:a 128k -ac 2 -f mp4 "tmp/${f}_350.mp4"
     tend="$(date "+%s")"
     runtime1="$(($tend - $tstart))"
-    echo " ---->>> Execution time = "$runtime1"ms"
+    runtime1="$(($runtime1 * 1000))"
+    echo " ---->>> Execution time = "$runtime1" s"
     ffmpeg -y -i "${fe}" -vsync passthrough -s 640x360 -c:v libx264 -b:v 650k -x264opts keyint=25:min-keyint=25:no-scenecut -profile:v main -preset slow -movflags +faststart -c:a aac -b:a 128k -ac 2 -f mp4 "tmp/${f}_650.mp4"
     tend="$(date "+%s")"
     runtime2="$(($tend - $tstart - $runtime1))"
-    echo " ---->>> Execution time = "$runtime2"ms"    
+    runtime2="$(($runtime2 * 1000))"
+    echo " ---->>> Execution time = "$runtime2" s"    
     ffmpeg -y -i "${fe}" -vsync passthrough -s 960x540 -c:v libx264 -b:v 1400k -x264opts keyint=25:min-keyint=25:no-scenecut -profile:v main -preset slow -movflags +faststart -c:a aac -b:a 128k -ac 2 -f mp4 "tmp/${f}_1400.mp4"
     tend="$(date "+%s")"
     runtime3="$(($tend - $tstart - $runtime1 - $runtime2))"
-    echo " ---->>> Execution time = "$runtime3"ms" 
+    runtime3="$(($runtime3 * 1000))"
+    echo " ---->>> Execution time = "$runtime3" s" 
     ffmpeg -y -i "${fe}" -vsync passthrough -s 1280x720 -c:v libx264 -b:v 2500k -x264opts keyint=25:min-keyint=25:no-scenecut -profile:v main -preset slow -movflags +faststart -c:a aac -b:a 128k -ac 2 -f mp4 "tmp/${f}_2500.mp4"
     tend="$(date "+%s")"
     runtime4="$(($tend - $tstart - $runtime1 - $runtime2 - $runtime3))"
-    echo " ---->>> Execution time = "$runtime4"ms" 
+    runtime4="$(($runtime4 * 1000))"
+    echo " ---->>> Execution time = "$runtime4" s" 
     ffmpeg -y -i "${fe}" -vsync passthrough -s 1920x1080 -c:v libx264 -b:v 5500k -x264opts keyint=25:min-keyint=25:no-scenecut -profile:v main -preset slow -movflags +faststart -c:a aac -b:a 128k -ac 2 -f mp4 "tmp/${f}_5500.mp4"
     tend="$(date "+%s")"
     runtime5="$(($tend - $tstart - $runtime1 - $runtime2 - $runtime3 - $runtime4))"
-    echo " ---->>> Execution time = "$runtime5"ms" 
+    runtime5="$(($runtime5 * 1000))"
+    echo " ---->>> Execution time = "$runtime5" s" 
 
     if [ -f "${fsrt}.srt" ]; then #if srt file exists, add captions
         echo "---> Adding subtitles to videos"
@@ -87,4 +87,5 @@ cd "$SAVEDIR"
 
 tend="$(date "+%s")"
 runtime="$(($tend - $tstart))"
-echo "Execution time = "$runtime"ms"
+runtime="$(($runtime * 1000))"
+echo "Execution time = "$runtime" s" # execution time in milis
