35512bb0b4bc864d8ef9f2a51a1bf585e9e3feb8
Guiamrey <guimarey.r@gmail.com>
2018-10-26 18:46:13 +0200

    Changes

--- manager/manager.py
+++ manager/manager.py
@@ -8,6 +8,7 @@ import os
 import time
 import logging
 import boto3
+
 ec2 = boto3.resource('ec2')
 ec2_client = boto3.client('ec2')
 launch_template_name = 'ffmpeg_instance'
@@ -37,14 +38,18 @@ def launch_proxy():
     )
     instance = response[0]
     instance_id = instance.id
+    global proxy_status
+    proxy_status = instance.state.get('Code')
     try:
-        while instance.state.get('Code') != 16:
+        while proxy_status != 16:
             time.sleep(5)
             instance.load()
-            logger.info('Status Proxy: ' + str(instance.state.get('Name')))
+            proxy_status = instance.state.get('Code')
+            logger.info('Status Proxy: ' + str(proxy_status))
 
-        response = ec2_client.associate_address(AllocationId='eipalloc-04c4e9298333c289e',
-                                                InstanceId=instance_id)
+        if instance.state.get('Code') == 16:
+            response = ec2_client.associate_address(AllocationId='eipalloc-04c4e9298333c289e',
+                                                    InstanceId=instance_id)
         logger.info('Elastic IP associated with proxy instance. Response: ' + str(response))
     except ClientError as e:
         logger.error('An error occurred while associating Elastic IP to Proxy instance. Error: ' + str(e.response))
@@ -54,22 +59,25 @@ def ping_manager():
     # check if main manager still up
     response = os.system('ping -c 1 ' + ip_manager)
     if response == 0:
-        logger.info('Main manager is still active.')
+        logger.info('Main manager is active.')
     else:
         logger.error('Main manager does not responde to PING.')
-        logger.info('Waking up second manager.')
+        logger.info('Waking up second manager...')
         global main_manager
         main_manager = True
 
 
 def ping_proxy():
     # check if proxy still up
+    global proxy_status
+    if proxy_status != 'running':
+        return
     response = os.system('ping -c 1 ' + ip_proxy)
     if response == 0:
-        logger.info('Proxy server is still active.')
+        logger.info('Proxy server is active.')
     else:
         logger.error('Proxy server does not responde to PING.')
-        logger.info('Waking up proxy.')
+        logger.info('Waking up proxy...')
         global proxy_up
         proxy_up = False
 
@@ -78,6 +86,7 @@ def ping_proxy():
 main_manager = (sys.argv[1] == 'True')
 region_name = 'eu-west-1'
 proxy_up = not main_manager
+proxy_status = ''
 ip_manager = sys.argv[2]
 ip_proxy = '52.17.18.108'
 
