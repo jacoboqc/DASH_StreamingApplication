98e8cdf0f48c9b15e93990b284373e5959e40de3
Guiamrey <guimarey.r@gmail.com>
2018-10-30 19:15:38 +0100

    Changes

--- manager/balancer.py
+++ manager/balancer.py
@@ -39,8 +39,6 @@ class Balancer(Thread):
             csv_file.flush()
             while True:
                 # Code status 16 = running / 0 = pending
-                size = sum(1 for ins in ec2.instances.all() if ins.state.get('Code') == 16)
-                logger.info('Running %s instances.' % str(size))
                 instances_running = 0
                 for instance in ec2.instances.all():
                     if instance.state.get('Code') == 16:
@@ -48,6 +46,7 @@ class Balancer(Thread):
                         if (ip != '52.17.18.108' and ip != '52.16.139.42' and ip != '34.247.193.119'
                                 and instance.id != 'i-0499fe00dbf35e2ae' and instance.id != 'i-0832e3e1c8cea1435'):
                             instances_running += 1
+                            logger.info('Running %s instances.' % str(instances_running))
 
                             cpu_load = self.cpu_metric(instance)
                             network_in = self.network_in(instance)
@@ -63,10 +62,6 @@ class Balancer(Thread):
                             now = datetime.datetime.now(timezone.utc)
                             difference = (now - launch_time).total_seconds()
                             logger.info('Total Time instance running (id: ' + str(instance.id) + ') - ' + str(difference))
-                            logger.info(int(difference) > 10*60)
-                            logger.info(int(instances_running) > 1)
-                            logger.info(float(cpu_load) < 5.0)
-                            logger.info(float(cpu_load) < 5.0 and int(instances_running) > 1 and int(difference) > 10*60)
                             if float(cpu_load) < 5.0 and int(instances_running) > 1 and int(difference) > 10*60:
                                 ec2_client.stop_instances(InstanceIds=[instance.id], DryRun=False)
                                 logger.info('Stopping instance %s for low load' % instance.id)
